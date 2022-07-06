af7f2ec38e7b3079af169f85031853bea5930718
Ron Owens <rowens72@yahoo.com>
2022-07-06 08:44:11 -0700

    simplified function

--- pf_practice4.py
+++ pf_practice4.py
@@ -1,11 +1,6 @@
 # Write a Python function called max_num()to find the maximum of three numbers.
 def max_num(a, b, c):
-    if(a >= b) and (a >= c):
-        return a
-    elif (b >= a) and (b >= c):
-        return b
-    else:
-        return c
+    return max(a, b, c)
 
 # Write a Python function called mult_list() to multiply all the numbers in a list.
 #def  mult_list():
@@ -14,7 +9,7 @@ def max_num(a, b, c):
 #def rev_string(): 
 
 # Write a Python function called num_within() to check whether a number falls in a given range.
-# The function accepts the number, beginning of range, and end of range (inclusive) as arguments.
+# The function accepts the number, bseginning of range, and end of range (inclusive) as arguments.
 # Examples: num_within(3,2,4) returns True, num_within(3,1,3) returns True, num_within(10,2,5) returns False.
 #def num_within(): 
 
